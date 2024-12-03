193b829ab9579e05e78937954e3aed8767bd2206
Jakub Koterba <kkoterba9@gmail.com>
2024-12-03 08:43:55 +0100

    removed unused dir

---
+++ 02_12_2024_java/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.puzzle_1.PuzzleOne;
+import org.puzzle_2.PuzzleTwo;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        var p1 = new PuzzleOne();
+        var p2 = new PuzzleTwo();
+
+        System.out.print("Result one: " + p1.solve_first() + "\n");
+        System.out.print("Result two: " + p2.solve_second());
+    }
+}
---
+++ 02_12_2024_java/src/main/java/org/puzzle_2/PuzzleTwo.java
@@ -0,0 +1,167 @@
+package org.puzzle_2;
+
+import org.puzzle_1.PuzzleOne;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class PuzzleTwo {
+
+    private String data;
+
+    public PuzzleTwo() {
+        try (var in = PuzzleOne.class.getResourceAsStream("/data.txt")) {
+            this.data = new String(in.readAllBytes(), Charset.defaultCharset());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /*
+    What are the cases when the error can be recovered?
+    - monotonicity change from rising to decreasing, then i can rollback from the current i and leave the n_i
+    - from decreasing to rising
+    - when the change is bigger than domain
+     */
+    public Boolean solve(ArrayList<Integer> d, int i) {
+        if (i > d.size() - 1) {
+            return false;
+        }
+
+        boolean result = safe(d);
+
+        d.remove(i);
+        return result ? result : solve(d, i);
+    }
+
+    public int solve_second() {
+
+        //
+        var safeSequences = 0;
+
+
+        ArrayList<ArrayList<Integer>> data = getArrayLists();
+
+        for(ArrayList<Integer> l: data) {
+
+            System.out.print("LINE\n");
+            if (solve(l, 0)) {
+                safeSequences++;
+            }
+        }
+
+        return safeSequences;
+    }
+
+    public int solve_one_and_a_half() {
+        //
+        var safeSequences = 0;
+
+
+        ArrayList<ArrayList<Integer>> data = getArrayLists();
+
+        // bylo 301
+
+        for(ArrayList<Integer> l: data) {
+
+            if(safe(l)) {
+                safeSequences++;
+            } else {
+                for (int i = 0; i < l.size(); i++) {
+                    int removed = l.remove(i);
+                    if (safe(l)) {
+                        safeSequences++;
+                        break;
+                    }
+                    l.add(i, removed);
+                }
+            }
+
+        }
+
+        return safeSequences;
+    }
+
+    public boolean safe(ArrayList<Integer> l) {
+
+            Set<Integer> incr = Stream.of(1, 2, 3)
+                    .collect(Collectors.toCollection(HashSet::new));
+
+            Set<Integer> decr = Stream.of(-1, -2, -3)
+                    .collect(Collectors.toCollection(HashSet::new));
+
+            for(int i = 1; i < l.size(); i++) {
+                var temp = l.get(i) - l.get(i-1);
+
+                incr.add(temp);
+                decr.add(temp);
+            }
+
+            return (incr.size() == 3 || decr.size() == 3);
+    }
+
+    private Boolean checkWithExclusions(ArrayList<Integer> l, int excluded) {
+        ArrayList<Integer> copy = (ArrayList<Integer>) l.clone();
+        copy.remove(excluded);
+
+        var rising = false;
+        var declining = false;
+
+        var is_safe = true;
+        for(int i = 1; i < copy.size();i++) {
+
+            var temp = l.get(i-1) - l.get(i);
+
+            if(i == 1) {
+                if (temp < 0) {
+                    declining = true;
+                } else {
+                    rising = true;
+                }
+                continue;
+            }
+
+            if (rising && temp < 0) {
+                is_safe = false;
+                break;
+            }
+            if (declining && temp > 0) {
+                is_safe = false;
+                break;
+            }
+
+            if(Math.abs(temp) < 1 || Math.abs(temp) > 3) {
+                is_safe = false;
+                break;
+            }
+
+        }
+
+        return is_safe;
+    }
+
+
+    private ArrayList<ArrayList<Integer>> getArrayLists() {
+        Scanner myReader = new Scanner(this.data);
+        ArrayList<ArrayList<Integer>> data = new ArrayList<>();
+
+        // Reading the data and calculating from the get go
+        while (myReader.hasNextLine()) {
+            String line = myReader.nextLine();
+
+            ArrayList<Integer> lineList = new ArrayList<>();
+
+            // Another reader for a string line
+            Scanner lineReader = new Scanner(line);
+            while(lineReader.hasNextInt()) {
+                lineList.add(lineReader.nextInt());
+            }
+
+            data.add(lineList);
+        }
+        return data;
+    }
+}
