dcf5766283dd6b142b7fadb921d0c1bab67bc2d9
Jakub Koterba <kkoterba9@gmail.com>
2024-12-03 21:29:24 +0100

    final day3

--- 03_12_2024_python/puzzle_1/iter_2.py
+++ 03_12_2024_python/puzzle_1/iter_2.py
@@ -4,8 +4,10 @@ def solve():
     # regexes?
     with open("../data.txt", "r") as f:
         test = 'xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))'
-        to_multiply = [re.findall(r'\d+', o) for o in re.findall(r'mul[(][0-9]{1,3},[0-9]{1,3}[)]', f.read())]
-        multiplications = ([int(entry[0]) * int(entry[1]) for entry in to_multiply])
-        print(sum(multiplications))
+        to_multiply = [
+            re.findall(r'\d+', o)
+            for o in re.findall(r'mul[(][0-9]{1,3},[0-9]{1,3}[)]', f.read())
+            ]
+        print(sum([int(entry[0]) * int(entry[1]) for entry in to_multiply]))
 
 solve()
