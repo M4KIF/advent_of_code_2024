fde1799cba6cfe0741daf31082c794d873783b36
Jakub Koterba <kkoterba9@gmail.com>
2024-12-10 21:09:06 +0100

    part2 was a struggle with regexes. Array permutations worked out tho

--- 09_12_2024_python/src/main.py
+++ 09_12_2024_python/src/main.py
@@ -1,5 +1,15 @@
-from solution import puzzle_1
+from solution import *
 
 if __name__ == "__main__":
-    runner = puzzle_1.Part1("../data_long.txt")
-    print(runner.solve())
+    runner_part_1 = Part1("../data.txt")
+    # runner_part_2_s = puzzle_2.Part2("../data.txt")
+    # print(runner_part_2_s.solve())
+
+
+    # That is the working one
+    # runner_part_2_b = Part2("../data_long.txt")
+    # print(runner_part_2_b.alternative())
+
+    # This I want to tinker with
+    runner_part_2_b = Part2("../data.txt")
+    print(runner_part_2_b.mess())
